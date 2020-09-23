67eb356f145624afd9cbdd4606564e01ef3976c8
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-09-23 18:13:53 -0300

    Mudanças na lógica de redirecionamento

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AuthProvider from './context/AuthContextProvider';
 import './App.css';
 import LoginPage from './pages/LoginPage';
@@ -7,15 +7,13 @@ import RegisterPage from './pages/Register/RegisterPage';
 import NavBar from './components/NavBar';
 
 function App() {
+  const path = window.location.pathname;
   return (
     <div className="App">
       <AuthProvider>
         <Router>
           <Switch>
-            <Redirect to="/login">
-              <Route exact path="/"/>
-            </Redirect>
-            <Route exact path="/login" component={ LoginPage } />
+            <Route exact path={ path === '/' || '/login' } component={ LoginPage } />
             <Route path="/register" component={ RegisterPage } />
             <Route path="/navbartest" component={ NavBar } />
           </Switch>
