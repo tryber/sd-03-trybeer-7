ded8c1447a408f99a924665356bb8d3165e07c8b
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-09-24 19:59:52 -0300

    Requisito 3

---
+++ front-end/src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Buttons(title, link, testId) {
+  return (
+    <div>
+      <Link to={link}>
+        <button data-testid={testId} type="button">
+          <p>{`${title}`}</p>
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default Buttons;
