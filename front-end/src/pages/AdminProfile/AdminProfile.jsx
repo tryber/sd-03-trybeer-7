26f1e772560ab4368c32edc0f5f4eb86e9d9baf2
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-09-25 16:53:11 -0300

    Admin feita faltando ajustar o navbar para o navbar de admin

---
+++ front-end/src/pages/AdminProfile/AdminProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
+
+function AdminProfile() {
+  const userData = JSON.parse(localStorage.getItem('user'));
+  if (!userData || userData.role !== "admin") return <Redirect to="/login" />;
+  return (
+    <div style={ { display: 'flex', flexDirection: 'column' } }>
+      <NavBar title="Meu perfil" />
+      <div>
+          <h2>Perfil</h2>
+          <h3 data-testid="profile-name">Nome: {userData.name}</h3>
+          <h3 data-testid="profile-email">Email: {userData.email}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default AdminProfile;
