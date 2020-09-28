c8f3a5b7c6ce8ef354d82b7b54718ebb142f076b
Hfreitas <hfreitas86@gmail.com>
2020-09-28 19:08:18 -0300

    prototipo clientOrders ok

--- front-end/src/pages/ClientOrders/ClientOrders.jsx
+++ front-end/src/pages/ClientOrders/ClientOrders.jsx
@@ -2,23 +2,39 @@ import React, { useState, useEffect, useContext } from 'react';
 import AuthContext from '../../context/AuthContext';
 import ClientNavBar from '../../components/NavBar/ClientBar/ClientNavBar';
 import OrderCard from '../../components/OrderCard';
+import { userOrders } from '../../services';
 
 function ClientOrders() {
   const { user } = useContext(AuthContext);
-  const [userId, setUserId] = useState(null);
   const [orders, setOrders] = useState(null);
+  const [errors, setErrors] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
-    effect
+    if (!user) return undefined;
+    setIsFetching(true);
+    userOrders(user.id).then(
+      (response) => {
+        setOrders(response);
+        setIsFetching(false);
+      },
+      (response) => {
+        setErrors(response);
+        setIsFetching(false);
+      },
+    );
     return () => {
-      cleanup
-    }
-  }, [input])
+      setOrders(null);
+      setErrors(null);
+      setIsFetching(false);
+    };
+  }, [user]);
 
   return (
     <div style={ { display: 'flex', flexDirection: 'column' } }>
       <ClientNavBar title="Meus Pedidos" />
-      {orders.map((order, index) => (
+      {errors && <h4>{errors}</h4>}
+      {!isFetching && orders && orders.map((order, index) => (
         <OrderCard
           key={ order.id }
           index={ index }
