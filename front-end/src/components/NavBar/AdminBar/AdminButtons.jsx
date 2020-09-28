145284f421eae71c99471a03528a0d1bcba216cf
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-09-28 18:19:27 -0300

    Corrigir datatestid

--- front-end/src/components/NavBar/AdminBar/AdminButtons.jsx
+++ front-end/src/components/NavBar/AdminBar/AdminButtons.jsx
@@ -4,7 +4,7 @@ import Button from '../Button';
 function AdminButtons() {
   return (
     <div>
-      {Button('Pedidos', '/admin/orders', 'side-menu-item-products')}
+      {Button('Pedidos', '/admin/orders', 'side-menu-item-orders')}
       {Button('Perfil', '/admin/profile', "side-menu-item-profile")}
       {Button('Sair', '/login', 'side-menu-item-logout')}
     </div>
