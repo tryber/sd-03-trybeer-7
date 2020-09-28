13d6f8592c1272dec72febf94a6c76a1c4aa74c3
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-09-28 17:47:45 -0300

    Navbar ajustado

--- front-end/src/pages/AdminProfile/AdminProfile.jsx
+++ front-end/src/pages/AdminProfile/AdminProfile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import NavBar from '../../components/NavBar';
+import AdminNavBar from '../../components/NavBar/AdminBar/AdminNavBar';
 
 function AdminProfile() {
   const userData = JSON.parse(localStorage.getItem('user'));
   if (!userData || userData.role !== "administrator") return <Redirect to="/login" />;
   return (
     <div style={ { display: 'flex', flexDirection: 'column' } }>
-      <NavBar title="Meu perfil" />
+      <AdminNavBar title="Meu perfil" />
       <div>
           <h2>Perfil</h2>
           <h3 data-testid="profile-name">Nome: {userData.name}</h3>
