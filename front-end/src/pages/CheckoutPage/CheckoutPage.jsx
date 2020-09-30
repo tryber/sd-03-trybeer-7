f795b1418ad819823a3dad8bbc791fedef950368
Hfreitas <hfreitas86@gmail.com>
2020-09-30 18:39:10 -0300

    checkoutcard component ok e ajustes eslint

--- front-end/src/pages/CheckoutPage/CheckoutPage.jsx
+++ front-end/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React, { useState, useEffect, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import ClientNavBar from '../../components/NavBar/ClientBar/ClientNavBar';
+import Button from '../../components/NavBar/Button';
+import AuthContext from '../../context/AuthContext';
 
 function Checkout() {
-  return <div></div>;
+  return (
+    <div>
+      <ClientNavBar title="Finalizar Pedido" />
+      <p>Total:</p>
+      {Button('Finalizar Pedido', null, 'checkout-finish-btn')}
+    </div>
+  );
 }
-
-Checkout.propTypes = {};
 
 export default Checkout;
