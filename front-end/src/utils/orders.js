1165a21f6da7d64bffc5c0d7f0cf9c661822b19f
Gabryel <gabryel.ryba@gmail.com>
2020-10-01 17:14:19 -0300

    Funcao separada do componente em ClientOrderCard

---
+++ front-end/src/utils/orders.js
@@ -0,0 +1,10 @@
+// Baseado em: https://stackoverflow.com/questions/3075577/convert-mysql-datetime-stamp-into-javascripts-date-format
+const convertMySQLDatetime = (date = '') => {
+  const initialDateIndex = 5;
+  const finalDateIndex = 10;
+  const extractDayAndMonth = date.slice(initialDateIndex, finalDateIndex).split('-').reverse()
+    .join('/');
+  return extractDayAndMonth;
+};
+
+export { convertMySQLDatetime };
